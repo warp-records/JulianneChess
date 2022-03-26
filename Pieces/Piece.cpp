5cb224fa6cee63e05a162fd342f07095fda7e402
Ethan <ethanstm8@gmail.com>
2022-03-26 12:38:29 -0400

    Revert "Implement gen diagonals"
    This reverts commit 88f659c9dab76506ff59d58d7cd6ba8e37fb1ec4.

--- Pieces/Piece.cpp
+++ Pieces/Piece.cpp
@@ -7,32 +7,17 @@ Pieces::Color operator!(Pieces::Color color) {
 
 void Piece::genStraightMoveData() {
 	Bitboard rangeBoard = 0x00;
-	//Up
 	rangeBoard |= genValidRange([](Bitboard b) { return b << 8; }, 7 - pos.row);
-	//Down
-	rangeBoard |= genValidRange([](Bitboard b) { return b >> 8; }, pos.row);
-	//Right
-	rangeBoard |= genValidRange([](Bitboard b) { return b >> 1; }, 7 - pos.column);
-	//Left
-	rangeBoard |= genValidRange([](Bitboard b) { return b << 1; }, pos.column);
 
-	cache.moveRange |= rangeBoard;
 	//THE PAGAN WINTER!
+
+	
+	return;
 }
 
 void Piece::genDiagonalMoveData() { 
 
-	Bitboard rangeBoard = 0x00;
-	//Up right
-	rangeBoard |= genValidRange([](Bitboard b) { return b << 7; }, std::min(7 - pos.column, 7 - pos.row));
-	//Up left
-	rangeBoard |= genValidRange([](Bitboard b) { return b << 9; }, std::min(pos.column, 7 - pos.row));
-	//Down right
-	rangeBoard |= genValidRange([](Bitboard b) { return b >> 9; }, std::min(7 - pos.column, pos.row));
-	//Down left
-	rangeBoard |= genValidRange([](Bitboard b) { return b >> 7; }, std::min(pos.column, pos.row));
-
-	cache.moveRange |= rangeBoard;
+	return; 
 }
 
 //Shift function: function for shifting tilemask
