9a6fadf54511acea8f23579a61b230e74fcfbfb4
Ethan <ethanstm8@gmail.com>
2022-03-26 12:38:18 -0400

    Revert "Revert "Partial implement move gen""
    This reverts commit f3b06b2d0c1de11553c446681ef475940549ba3f.
    
    Revert

--- Pieces/Queen.cpp
+++ Pieces/Queen.cpp
@@ -5,9 +5,4 @@ namespace Pieces {
 	Bitboard Queen::getMoveRange() {
 		return MoveTables::Queen[pos.column][pos.row];
 	}
-
-	std::array<Pos, 8> Queen::genAttackList(GameBoard const& gb) {
-		//Implement later
-		return std::array<Pos, 8> {};
-	}
 }
